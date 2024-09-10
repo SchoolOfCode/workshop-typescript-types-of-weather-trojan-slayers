ac4fe0e707163f44fa2f4951d46a4a16e83fbe3c
Shilpa <eclectic89@hotmail.com>
2024-09-10 12:45:56 +0100

    Error handling added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,32 @@
   import { useEffect, useState } from "react";
   import { createClient } from "@supabase/supabase-js";
 
-  const supabase = createClient(" https://dosmthtitslppqluzxzu.supabase.co", "<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvc210aHRpdHNscHBxbHV6eHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjQ5ODgsImV4cCI6MjA0MTU0MDk4OH0.w2zvUM9CU4zrp7p-pwWmMbN-y3IY0ugKCpj2cjav5ii>")
+  const supabase = createClient(" https://dosmthtitslppqluzxzu.supabase.co", "<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvc210aHRpdHNscHBxbHV6eHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjQ5ODgsImV4cCI6MjA0MTU0MDk4OH0.w2zvUM9CU4zrp7p-pwWmMbN-y3IY0ugKCpj2cjav5iA>")
 
   function App() {
     const [countries, setCountries] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [error, setError] =useState(null);
+    
 
     useEffect(() => {
       getCountries();
     }, []);
 
     async function getCountries() {
-      const { data } = await supabase.from("countries").select();
+      try {
+      const { data, error } = await supabase.from("countries").select();
+      if(error) throw error
       setCountries(data);
+    } catch(error){
+      console.log("Error fetching", error);
+      setError("Failed Fetching ");
+    } finally {
+      setLoading(false);
     }
+  }
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
 
     return (
       <ul>
